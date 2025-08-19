d9685ea06118752930da56b1cc8307dce54f8457
karthiraja20596 <karthiraja20596@gmail.com>
2025-08-19 11:51:15 +0530

    new project

---
+++ loops.js
@@ -0,0 +1,32 @@
+//for loop
+//syntax
+
+for(let a=1;a<=5;a++){
+    console.log(a)
+}
+//task 1
+for(let b=1;b<5;b++){
+    console.log("karthi")
+}
+//while loop
+let b=1
+while(b<=5){
+    console.log(b);
+    b++
+}
+//do while loop
+let c=1
+do{
+    console.log(c)
+    c++
+
+}while(c<=5)
+for(let a=5;a==0;a--){
+    console.log(a)
+}
+let d=5
+do{
+    console.log(d)
+    d--
+
+}while(d>=1)
